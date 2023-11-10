4da4ccdec8d33578f18698a204e44690af65e0ec
Scorpionslap2 <71413189+Scorpionslap2@users.noreply.github.com>
2023-11-10 16:06:11 -0300

    repair 2

--- Proyecto/proyecto/src/app/app-routing.module.ts
+++ Proyecto/proyecto/src/app/app-routing.module.ts
@@ -5,18 +5,12 @@ import { IngresarComponent } from './ingresar/ingresar.component';
 import { RegistrarseComponent } from './registrarse/registrarse.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { AcercadeComponent } from './acerca de/acercade.component';
-import { AgendarComponent } from './agendar/agendar.component';
-import { UsuarioRComponent } from './usuarioRegistrado/usuarioR.component';
-import { UsuarioAComponent } from './usuarioAdministrador/usuarioA.component';
 
 const routes: Routes = [
   { path: 'ingresar', component: IngresarComponent },
   { path: 'registrarse', component: RegistrarseComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'acercade', component: AcercadeComponent },
-  { path: 'agendar', component: AgendarComponent},
-  { path: 'miagenda', component: UsuarioRComponent},
-  { path: 'misreservas', component: UsuarioAComponent},
 ];
 
 @NgModule({
