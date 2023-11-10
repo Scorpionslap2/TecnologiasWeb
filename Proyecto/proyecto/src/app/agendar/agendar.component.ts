24f11bf658b552523653dc21268d9f666b9a42e5
AylineSanchez <ayline.07.01@gmail.com>
2023-11-10 14:28:16 -0300

    Cambio en Agendar

--- Proyecto/proyecto/src/app/agendar/agendar.component.ts
+++ Proyecto/proyecto/src/app/agendar/agendar.component.ts
@@ -1,19 +1,48 @@
-import { Component, inject } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+//import { ApiService } from './services/api.service'; // Asumiendo que tienes un servicio llamado ApiService
 
 @Component({
   selector: 'app-agendar',
   templateUrl: './agendar.component.html',
   styleUrls: ['./agendar.component.css']
 })
-export class AgendarComponent {
-  private breakpointObserver = inject(BreakpointObserver);
+export class AgendarComponent implements OnInit {
+  professions: any[] = [];
+  professionals: any[] = [];
+  selectedProfession: string = '';
+  selectedProfessional: string = '';
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.getProfessions();
+    this.getProfessionals();
+  }
+
+  getProfessions(): void {
+    this.apiService.getProfessions().subscribe(
+      (data: any) => {
+        this.professions = data;
+      },
+      (error: any) => {
+        console.error('Error al obtener profesiones', error);
+      }
     );
+  }
+
+  getProfessionals(): void {
+    this.apiService.getProfessionals().subscribe(
+      (data: any) => {
+        this.professionals = data;
+      },
+      (error: any) => {
+        console.error('Error al obtener profesionales', error);
+      }
+    );
+  }
+
+  confirmarReserva(): void {
+    // Lógica para confirmar la reserva y enviar los datos al backend
+    // Puedes utilizar this.selectedProfession, this.selectedProfessional y otros datos del formulario
+  }
 }
