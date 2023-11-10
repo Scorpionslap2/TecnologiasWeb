b0aa11b6a6382c206ee56e850ae668e163279769
Scorpionslap2 <71413189+Scorpionslap2@users.noreply.github.com>
2023-11-10 17:47:54 -0300

    repair 3

--- Proyecto/proyecto/src/app/app.module.ts
+++ Proyecto/proyecto/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,14 +9,16 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AcercadeComponent } from './acerca de/acercade.component';
-import { ContactoComponent } from './contacto/contacto.component';
-import { IngresarComponent } from './ingresar/ingresar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+
+import { AppComponent } from './app.component';
 import { RegistrarseComponent } from './registrarse/registrarse.component';
+import { AcercadeComponent } from './acerca de/acercade.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { IngresarComponent } from './ingresar/ingresar.component';
 
 @NgModule({
   declarations: [
--- Proyecto/proyecto/src/app/registrarse/registrarse.component.ts
+++ Proyecto/proyecto/src/app/registrarse/registrarse.component.ts
@@ -1,19 +1,18 @@
-import { Component, inject } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-registrarse',
   templateUrl: './registrarse.component.html',
   styleUrls: ['./registrarse.component.css']
 })
-export class RegistrarseComponent {
-  private breakpointObserver = inject(BreakpointObserver);
+export class RegistrarseComponent implements OnInit{
+  constructor(private router: Router){
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  }
+
+  ngOnInit(){
+
+  }
+
 }
